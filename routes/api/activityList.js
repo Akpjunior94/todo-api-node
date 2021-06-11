7276e65a360f065b72023629d5b3f35d7b13b658
Akpan Abraham <akpjunio4real@gmail.com>
2021-06-11 03:59:34 -0700

    activity route updated

--- routes/api/activityList.js
+++ routes/api/activityList.js
@@ -2,37 +2,37 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
 // const uuid = require('uuid');
-const activitiesData = require('../../models/ActivityList');
+const ActivityList = require('../../models/ActivityList');
 // let verifyToken = require('../auth/verifyToken')
 
 //Get All Activities
-router.get('/', (req, res) => {
-
-  res.send(activitiesData);
+router.get('/', async (req, res) => {
+  try {
+    const activityLists = await ActivityList.find()
+    res.json(activityLists)
+  } catch (err) {
+    res.status(500).json({message: err.message})
+  }
 
 });
 
 // CREATING NEW ACTIVITY
-router.post('/', (req, res) => {
-
-  const newActivity = {
+router.post('/', async (req, res) => {
+  const activityList = new ActivityList ( {
     activity: req.body.activity,
-    week: req.body.week,
+    week : req.body.week,
     day: req.body.day,
     month: req.body.month,
     isCompleted: req.body.isCompleted
-  }
-   console.log({activitiesData, newActivity})
+  })
 
-  const activityCheck = activitiesData.some(user => user.activity === req.body.activity)
-
-  if (activityCheck) {
-    res.sendStatus(400);
-  } else {
-    activitiesData.push(newActivity)
-    res.json({msg:`The Activity: ${newActivity.activity} has been added`,
-    activitiesData})
+  try {
+    const newActivityList = await activityList.save()
+    res.status(201).json(newActivityList)
+  } catch (err) {
+    res.status(400).json({message: err.message})
   }
+
 })
 
 // view activities, day, week and month
@@ -40,9 +40,9 @@ router.get('/:id', (req, res)=> {
   const data = req.params.id
   if(!data) return res.send('');
 
-  const dayTodolist = activitiesData.filter(activityList => activityList.day === data)
-  const weekTodolist = activitiesData.filter(activityList => activityList.week === data)
-  const monthTodolist = activitiesData.filter(activityList => activityList.month === data)
+  const dayTodolist = ActivityList.filter(activityList => activityList.day === data)
+  const weekTodolist = ActivityList.filter(activityList => activityList.week === data)
+  const monthTodolist = ActivityList.filter(activityList => activityList.month === data)
   if (dayTodolist.length || weekTodolist.length || monthTodolist.length) {
     res.status(200).send({ success: true, data: { day:[...dayTodolist], week: [...weekTodolist], month: [...monthTodolist] }, message: `your ${data} todolist is here` })
   } else {
